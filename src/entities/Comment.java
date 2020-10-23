4949e42401cf8da78885fc4948415e2c8e4d70f3
Romeu Passadore N <passadore.neto@outlook.com>
2020-10-23 10:16:42 -0300

    Projeto Criado

---
+++ src/entities/Comment.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Comment {
+	private String tex;
+
+	public Comment() {
+	}
+
+	public Comment(String tex) {
+
+		this.tex = tex;
+	}
+
+	public String getTex() {
+		return tex;
+	}
+
+	public void setTex(String tex) {
+		this.tex = tex;
+	}
+
+}
